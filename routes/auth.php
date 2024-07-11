e9f7d7683ba24a55860cb8eeb1cc20f67341bcea
Sergio RD <117117684+nov4cs16@users.noreply.github.com>
2024-07-11 14:04:57 -0300

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -19,12 +19,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['guest', 'throttle:global'])->group(function () {
+    
     Route::get('register', [RegisteredUserController::class, 'create'])
                 ->name('register');
 
-//    Route::post('register', [RegisteredUserController::class, 'store']);
     Route::post('register', [RegisteredUserController::class, 'store'])->middleware('throttle:register');
-
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
